2bf0f207b66d51089b70dffafbfacb0cea7f1a7c
vokerchamp <102459238+vokerchamp@users.noreply.github.com>
2023-01-20 15:52:09 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+//my task is decalre 3 variables with complete sequance and my info.
+
+const myInfo ="Ali info";
+
+let myAddess ="kb colony";
+
+let myNumber ="03215144149";
+
+let myEmail ="hassamthechamp";
+
+let myCurrentlocation;
+
+console.table([myInfo,myAddess,myNumber,myEmail,myCurrentlocation]);
---
+++ 01_basics/primitive_datatypes.js
@@ -0,0 +1,18 @@
+// 7 primitive data types in javascript.
+
+let variable = "string" //String
+
+let variables = 12; //number
+
+// bigint use for big numbers.
+
+// Null
+
+// undifiend
+
+//boolean = either true or false.
+
+//symbol
+
+
+console.table(["string",12,null,true,false,])
