bac4f1c7fa84117cc3af0b87540ac7865ac20167
vokerchamp <102459238+vokerchamp@users.noreply.github.com>
2023-01-21 07:32:53 +0000

    coversionOperation

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,18 @@
+"use strict"; // Treat all javascript code as newer version of javascript.
+
+let umerage = 15;
+
+console.log(typeof umerage);
+
+let stringtonum  = Boolean(umerage);
+
+console.log(typeof stringtonum);
+
+console.log(stringtonum);
+
+// digits that are only contains digits can convert to number from string for example "20" => 20.
+// strings that contatins alphbet or something else with digits cannot convert into number for example "20abc" => Nan. stands for not a number.
+
+
+// when convert to Boolean 1 stand for true 0 stands for false "" empyt string stands for false.
+// number as 15 when convert to Boolean stands for true.
