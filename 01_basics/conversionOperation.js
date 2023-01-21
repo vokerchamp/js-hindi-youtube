6a9e7d56bafff672ba53847730a2536352e81100
vokerchamp <102459238+vokerchamp@users.noreply.github.com>
2023-01-21 21:35:58 +0000

    ecma

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -1,14 +1,14 @@
 "use strict"; // Treat all javascript code as newer version of javascript.
 
-let umerage = 15;
+// let umerage = 15;
 
-console.log(typeof umerage);
+// console.log(typeof umerage);
 
-let stringtonum  = Boolean(umerage);
+// let stringtonum  = Boolean(umerage);
 
-console.log(typeof stringtonum);
+// console.log(typeof stringtonum);
 
-console.log(stringtonum);
+// console.table([stringtonum,umerage ]);
 
 // digits that are only contains digits can convert to number from string for example "20" => 20.
 // strings that contatins alphbet or something else with digits cannot convert into number for example "20abc" => Nan. stands for not a number.
@@ -16,3 +16,33 @@ console.log(stringtonum);
 
 // when convert to Boolean 1 stand for true 0 stands for false "" empyt string stands for false.
 // number as 15 when convert to Boolean stands for true.
+
+// // let value ="6";
+
+// // let myvalue = "4";
+
+// // let nagvalue = value -myvalue;
+
+// // let nagValue =String(nagvalue);
+
+// // console.log(typeof nagValue);
+
+// // console.log(typeof nagvalue)
+
+
+
+console.log(Boolean > 0);
+console.log(Boolean == 1);
+console.log(Boolean < 1);
+
+
+
+
+
+
+
+
+
+
+
+
